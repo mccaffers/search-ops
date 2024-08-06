bc0d7cf83c20dc56922a9ab766da1773a0a5cc0d
Ryan <16667079+mccaffers@users.noreply.github.com>
2024-08-06 08:30:16 +0100

    Fixed merge conflict

--- Sources/SearchOps/DataManagers/SearchHistoryDataManager.swift
+++ Sources/SearchOps/DataManagers/SearchHistoryDataManager.swift
@@ -61,7 +61,6 @@ public class SearchHistoryDataManager: ObservableObject {
     return staticList().sorted(by: {$0.date > $1.date})
   }
   
-<<<<<<< HEAD
   // If you delete the host, lets delete all it's history
   public func deleteByHost(id: UUID) {
     refresh()
@@ -75,19 +74,6 @@ public class SearchHistoryDataManager: ObservableObject {
       }
     }
   }
-=======
-//  private func isSameDate(previous: SearchEvent, for event: SearchEvent, at index: Int) -> Bool {
-//    if index == 0 {
-//      return true
-//    }
-//    
-//    if areDatesSameDay(date1: previous.date, date2: event.date) {
-//      return false
-//    } else {
-//      return true
-//    }
-//  }
->>>>>>> main
   
   public func groupByDate() -> Dictionary<Date, [SearchEvent]> {
 
