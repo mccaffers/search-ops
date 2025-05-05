83f01ac6c90ce8c2435ab1152c5b76f207dc9e84
Ryan <16667079+mccaffers@users.noreply.github.com>
2025-05-05 17:53:12 +0100

    Fixed merge conflict

--- Source/Platforms/iOS/Settings/TechnicalSettings/TechnicalSettings.swift
+++ Source/Platforms/iOS/Settings/TechnicalSettings/TechnicalSettings.swift
@@ -53,30 +53,18 @@ struct TechnicalSettings: View {
           .cornerRadius(5.0)
         
       })
-<<<<<<< HEAD
       
-=======
-
->>>>>>> main
       Spacer()
       
     }
     .padding(.horizontal, 20)
     .navigationTitle("Database Settings")
     .toolbarBackground(Color.pink,
-<<<<<<< HEAD
                        for: .navigationBar)
     
     .frame(maxWidth: .infinity)
     .background(Color("Background"))
     
-=======
-                 for: .navigationBar)
-         
-    .frame(maxWidth: .infinity)
-    .background(Color("Background"))
-
->>>>>>> main
   }
 }
 
